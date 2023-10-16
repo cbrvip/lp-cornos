c9d3fa1052195b945ed9c4fa1130140d33eaf222
Gustavo Colhado <gustavocolhado@gmail.com>
2023-10-16 13:48:47 -0300

    teste

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="./src/assets/logo.png" alt="" width={150} height={45} />
+      <header>
+        
+        <nav>
+          <ul>
+            <li><a href="#">CONFISSÕES</a></li>
+            <li><a href="#">CORNOS VIP</a></li>
+            <li><a href="#">TWITTER</a></li>
+            <li><a href="#">TELEGRAM</a></li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <section className="hero">
+          <h1>Gosta de conteudo Cuckold ?</h1>
+          <p>Se você é um corno ou adora ver um video de corno acesse nosso conteudo!</p>
+          <a href="#" className="cta-button">Acesse nosso Conteudo</a>
+        </section>
+        <section className="features">
+          <div className="feature">
+            <h2>Videos de Corno</h2>
+            <p>Corno manso ✓ Videos de cornos que adoram ver suas esposas sendo comidas por amigos, gostam de ver suas esposas socando e gemendo bem gostoso!</p>
+          </div>
+          <div className="feature">
+            <h2>Conteudo Amador</h2>
+            <p>Assista vídeos pornô de Corno de graça, aqui no Pornhub.com. Descubra a crescente coleção de vídeos e filmes Mais relevantes explícitos em alta qualidade.</p>
+          </div>
+          <div className="feature">
+            <h2>+ de 22 mil Videos</h2>
+            <p>Atualização diária com vídeo de corno amador do Brasil. Coletânea completa com vídeos de corno pra você fantasiar e gozar muito.</p>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <div className="footer-content">
+          <p>&copy; 2023 Videos de Corno</p>
+          <div className="social-links">
+            <a href="#"><i className="fab fa-facebook"></i></a>
+            <a href="#"><i className="fab fa-twitter"></i></a>
+            <a href="#"><i className="fab fa-instagram"></i></a>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
